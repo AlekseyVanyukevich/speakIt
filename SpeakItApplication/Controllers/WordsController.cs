5c6c10f19a9655d716f5ce653abfabc8fe316f2e
leha.vanyukevich <leha.vanyukevich@gmail.com>
2020-06-25 00:43:28 +0300

    Add project files.

---
+++ SpeakItApplication/Controllers/WordsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SpeakItApplication.Models;
+
+namespace SpeakItApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordsController : ControllerBase
+    {
+        private readonly HttpClient _client;
+        public WordsController(HttpClient client)
+        {
+            _client = client;
+        }
+        [HttpGet("{level}")]
+        public async Task<IActionResult> GetWords(int level)
+        {
+            Random random = new Random();
+            HttpResponseMessage response = await _client.GetAsync($"?page={random.Next(29)}&group={level}");
+            if (!response.IsSuccessStatusCode)
+            {
+                return BadRequest();
+            }
+            string responseData = await response.Content.ReadAsStringAsync();
+            List<WordModel> words = JsonConvert.DeserializeObject<List<WordModel>>(responseData);
+            return Ok(words);
+        }
+    }
+}
---
+++ SpeakItApplication/Models/Word.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeakItApplication.Models
+{
+    public class WordModel
+    {
+        public string Id { get; set; }
+        public string Word { get; set; }
+        public string Image { get; set; }
+        public string Audio { get; set; }
+        public string Transcription { get; set; }
+        public string WordTranslate { get; set; }
+
+    }
+}
